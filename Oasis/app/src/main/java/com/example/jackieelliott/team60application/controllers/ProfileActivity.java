491b4742e6b1cb819f7d1a57e7e46cd63996e487
chess7 <chess7@gatech.edu>
2017-02-18 17:26:49 -0500

    Added profile page

--- Oasis/app/src/main/java/com/example/jackieelliott/team60application/controllers/ProfileActivity.java
+++ Oasis/app/src/main/java/com/example/jackieelliott/team60application/controllers/ProfileActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.jackieelliott.team60application.Model.Admin;
 import com.example.jackieelliott.team60application.Model.Manager;
@@ -15,16 +17,21 @@ import com.example.jackieelliott.team60application.Model.User;
 import com.example.jackieelliott.team60application.Model.Worker;
 import com.example.jackieelliott.team60application.R;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 /**
  * Created by JackieElliott on 2/12/17.
  */
 
-public class HomeActivity extends Activity {
+public class ProfileActivity extends Activity {
 
-    Button logoutButton;
-    Button profileButton;
+    Button backButton;
+    EditText email;
+    EditText homeAddress;
+    TextView username;
+    TextView accountType;
     ArrayList<User> userList;
     ArrayList<Worker> workerList;
     ArrayList<Manager> managerList;
@@ -33,49 +40,32 @@ public class HomeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.profile_page);
         addListenerOnButtonLogout();
         Bundle b = getIntent().getExtras();
         userList = b.getParcelableArrayList("UserList");
         workerList = b.getParcelableArrayList("WorkerList");
         managerList = b.getParcelableArrayList("ManagerList");
         adminList = b.getParcelableArrayList("AdminList");
+        //username.setText();
     }
 
     public void addListenerOnButtonLogout() {
 
         final Context context = this;
 
-        logoutButton = (Button) findViewById(R.id.logout_button);
+        backButton = (Button) findViewById(R.id.backButton);
+        email = (EditText) findViewById(R.id.emailText);
+        homeAddress = (EditText) findViewById(R.id.addressText);
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-
-
-                Intent intent = new Intent(context, WelcomePageActivity.class);
-                intent.putParcelableArrayListExtra("UserList", userList);
-                intent.putParcelableArrayListExtra("WorkerList", workerList);
-                intent.putParcelableArrayListExtra("ManagerList", managerList);
-                intent.putParcelableArrayListExtra("AdminList", adminList);
-                startActivity(intent);
-
-            }
-
-        });
-
-        profileButton = (Button) findViewById(R.id.button);
-
-        profileButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
 
 
 
-                Intent intent = new Intent(context, ProfileActivity.class);
+                Intent intent = new Intent(context, HomeActivity.class);
                 intent.putParcelableArrayListExtra("UserList", userList);
                 intent.putParcelableArrayListExtra("WorkerList", workerList);
                 intent.putParcelableArrayListExtra("ManagerList", managerList);
