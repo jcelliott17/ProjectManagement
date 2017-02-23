d86a290abe2c93845a9b40305a6a1d4ea304367d
Andrew Wireman <AWIREMAN@lawn-128-61-118-116.lawn.gatech.edu>
2017-02-22 20:28:03 -0500

    adding Python Virtualenv for django web app

---
+++ OasisWeb/OasisWeb/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/AWIREMAN/Documents/ProjectManagement/OasisWeb/OasisWeb/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
