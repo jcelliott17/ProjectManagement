70d136ba04c8ea1affe0840b7eb10f7159a27c56
Andrew Wireman <AWIREMAN@lawn-128-61-118-116.lawn.gatech.edu>
2017-02-22 20:28:55 -0500

    sa

--- OasisWeb/OasisWeb/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/AWIREMAN/Documents/ProjectManagement/OasisWeb/OasisWeb/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
