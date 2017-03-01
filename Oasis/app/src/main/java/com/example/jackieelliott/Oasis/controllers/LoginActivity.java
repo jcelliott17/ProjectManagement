9a61d4b3c4e5371ab91c7e2d2c1b5b1b110ef6a5
Butler <jbutler72@gatech.edu>
2017-03-01 17:10:16 -0500

    User authentication is set up. Firebase versions are now 9.8.

--- Oasis/app/src/main/java/com/example/jackieelliott/Oasis/controllers/LoginActivity.java
+++ Oasis/app/src/main/java/com/example/jackieelliott/Oasis/controllers/LoginActivity.java
@@ -5,10 +5,13 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
 import android.content.DialogInterface;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,6 +20,13 @@ import com.example.jackieelliott.Oasis.Model.Manager;
 import com.example.jackieelliott.Oasis.Model.User;
 import com.example.jackieelliott.Oasis.Model.Worker;
 import com.example.jackieelliott.Oasis.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by JackieElliott on 2/8/17.
@@ -33,6 +43,12 @@ public class LoginActivity extends Activity {
     ArrayList<Manager> managerList;
     ArrayList<Admin> adminList;
 
+    private static final String TAG = "LoginActivity-TAG";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mDatabase;
+    private DatabaseReference accountEndPoint;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +60,24 @@ public class LoginActivity extends Activity {
         workerList = b.getParcelableArrayList("WorkerList");
         managerList = b.getParcelableArrayList("ManagerList");
         adminList = b.getParcelableArrayList("AdminList");
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        accountEndPoint = mDatabase.child("accounts");
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
 
     public void addListenerOnButtonLogin() {
@@ -68,7 +101,19 @@ public class LoginActivity extends Activity {
 
             @Override
             public void onClick(View arg0) {
-                boolean login = false;
+                //boolean login = false;
+                if (validateForm()) {
+                    signIn();
+                    Intent intent = new Intent(context, HomeActivity.class);
+                    intent.putParcelableArrayListExtra("UserList", userList);
+                    intent.putParcelableArrayListExtra("WorkerList", workerList);
+                    intent.putParcelableArrayListExtra("ManagerList", managerList);
+                    intent.putParcelableArrayListExtra("AdminList", adminList);
+                    startActivity(intent);
+                } else {
+                    alertDialog.show();
+                }
+                /*
                 for (int i = 0; i < userList.size(); i++) {
                     if (userList.get(i).getUsername().equals(loginField.getText().toString())
                             && passField.getText().toString().equals(userList.get(i).getPassword())) {
@@ -99,6 +144,7 @@ public class LoginActivity extends Activity {
                         }
                     }
                 }
+
                 if (login) {
                     Intent intent = new Intent(context, HomeActivity.class);
                     intent.putParcelableArrayListExtra("UserList", userList);
@@ -109,6 +155,7 @@ public class LoginActivity extends Activity {
                 } else {
                     alertDialog.show();
                 }
+                */
 
             }
 
@@ -140,6 +187,34 @@ public class LoginActivity extends Activity {
 
     }
 
+    private void signIn() {
+        String email = loginField.getText().toString();
+        String password = passField.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        return (isEmailValid(loginField.getText().toString())
+            && isPasswordValid(passField.getText().toString()));
+    }
+
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
         return email.contains("@");
@@ -148,5 +223,19 @@ public class LoginActivity extends Activity {
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 4;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- Oasis/app/src/main/java/com/example/jackieelliott/Oasis/controllers/RegisterUserActivity.java
+++ Oasis/app/src/main/java/com/example/jackieelliott/Oasis/controllers/RegisterUserActivity.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 import com.example.jackieelliott.Oasis.Model.AccountTypes;
@@ -17,6 +22,14 @@ import com.example.jackieelliott.Oasis.Model.Worker;
 import com.example.jackieelliott.Oasis.Model.Manager;
 import com.example.jackieelliott.Oasis.Model.Admin;
 import com.example.jackieelliott.Oasis.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by JackieElliott on 2/8/17.
@@ -27,12 +40,19 @@ public class RegisterUserActivity extends Activity{
     Button registerBotton;
     Button cancelButton;
     private Spinner accountTypeSpinner;
-    EditText userNameField;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "RegUserActivity-TAG";
+    private DatabaseReference mDatabase;
+    private DatabaseReference accountEndPoint;
+    private Context context;
+    EditText emailField;
     EditText passField;
     ArrayList<User> userList;
     ArrayList<Worker> workerList;
     ArrayList<Manager> managerList;
     ArrayList<Admin> adminList;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,46 +72,135 @@ public class RegisterUserActivity extends Activity{
         workerList = b.getParcelableArrayList("WorkerList");
         managerList = b.getParcelableArrayList("ManagerList");
         adminList = b.getParcelableArrayList("AdminList");
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        accountEndPoint = mDatabase.child("accounts");
+        mAuth = FirebaseAuth.getInstance();
 
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                // updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
+    }
+
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    // [END on_stop_remove_listener]
+
+    private void createAccount(String email, String password) {
+        accountEndPoint.setValue(email + " - " + password);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegisterUserActivity.this, "Authentication failed: " + task.getException().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterUserActivity.this, "You're in!",
+                                    Toast.LENGTH_SHORT).show();
+                            if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.User) {
+                                User user = new User(emailField.getText().toString(), passField.getText().toString());
+                                userList.add(user);
+                            } else if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.Worker) {
+                                Worker worker = new Worker(emailField.getText().toString(), passField.getText().toString());
+                                workerList.add(worker);
+                            } else if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.Manager) {
+                                Manager manager = new Manager(emailField.getText().toString(), passField.getText().toString());
+                                managerList.add(manager);
+                            } else if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.Admin) {
+                                Admin admin = new Admin(emailField.getText().toString(), passField.getText().toString());
+                                adminList.add(admin);
+                            }
+                            Intent intent = new Intent(context, HomeActivity.class);
+                            intent.putParcelableArrayListExtra("UserList", userList);
+                            intent.putParcelableArrayListExtra("WorkerList", workerList);
+                            intent.putParcelableArrayListExtra("ManagerList", managerList);
+                            intent.putParcelableArrayListExtra("AdminList", adminList);
+                            startActivity(intent);
+                        }
+                        //task.
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+
+        boolean valid = true;
+
+        String email = emailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailField.setError("Required.");
+            valid = false;
+        } else {
+            emailField.setError(null);
+        }
+
+        String password = passField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passField.setError("Required.");
+            valid = false;
+        } else {
+            passField.setError(null);
+        }
+        return valid;
     }
 
     public void addListenerOnButtonRegister() {
 
-        final Context context = this;
+        context = this;
 
         registerBotton = (Button) findViewById(R.id.registerOnRegisterPage);
-        userNameField = (EditText) findViewById(R.id.username_text);
+        emailField = (EditText) findViewById(R.id.username_text);
         passField = (EditText) findViewById(R.id.editText3);
-
 
         registerBotton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
 
-                if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.User) {
-                    User user = new User(userNameField.getText().toString(), passField.getText().toString());
-                    userList.add(user);
-                } else if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.Worker) {
-                    Worker worker = new Worker(userNameField.getText().toString(), passField.getText().toString());
-                    workerList.add(worker);
-                } else if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.Manager) {
-                    Manager manager = new Manager(userNameField.getText().toString(), passField.getText().toString());
-                    managerList.add(manager);
-                } else if (accountTypeSpinner.getSelectedItem() == AccountTypes.AccountType.Admin) {
-                    Admin admin = new Admin(userNameField.getText().toString(), passField.getText().toString());
-                    adminList.add(admin);
+                if (validateForm()) {
+                    createAccount(emailField.getText().toString(), passField.getText().toString());
+                } else {
+                    Log.d(TAG, "Failed validation");
                 }
-                Intent intent = new Intent(context, HomeActivity.class);
-                intent.putParcelableArrayListExtra("UserList", userList);
-                intent.putParcelableArrayListExtra("WorkerList", workerList);
-                intent.putParcelableArrayListExtra("ManagerList", managerList);
-                intent.putParcelableArrayListExtra("AdminList", adminList);
-                startActivity(intent);
+
             }
 
         });
-
     }
 
     public void addListenerOnButtonCancel() {
